5a824e2b483a9f68f21c105aeb43ee7a0fa8759c
Адексей <manay.ru@mail.ru>
2023-09-23 12:22:54 +1000

    Урок: 2 от GB Windows Form

---
+++ MyUtilites/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUtilites
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа мои утилиты, содержит ряд небольших программ, " +
+                "которые могут пригодиться в жизни. А главное научить меня основам программирования на C#" +
+                "Автор: Щербаков А.А.", "о программе");
+        }
+    }
+}
